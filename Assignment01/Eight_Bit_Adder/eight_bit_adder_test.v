cfea2fa1efb92720b6f83f8f110940e651bc648a
hitesh-anand <82741231+hitesh-anand@users.noreply.github.com>
2022-01-21 22:21:55 +0530

    Update eight_bit_adder_test.v
    made some minor changes and successfully tested the code

--- Assignment01/Eight_Bit_Adder/eight_bit_adder_test.v
+++ Assignment01/Eight_Bit_Adder/eight_bit_adder_test.v
@@ -1,51 +1,69 @@
+`include "eight_bit_adder.v"
+
 module eight_bit_adder_test;
 
-    // Taking the inputs
     reg [7:0] A;
     reg [7:0] B;
     reg Cin;
 
-    // Taking the outputs
     wire [7:0] Sum;
     wire Carry;
 
-    eight_bit_adder ADDER (A, B, Cin, Sum, Carry);
+    eight_bit_adder ADDER (A, B, Cin, Carry, Sum);
 
-    // Displaying the output
     always @ (A or B or Cin or Sum or Carry) begin
-        $display("<%d>: Input_A = %b, Input_B = %b, Input_Cin = %b, Sum = %b, Carry = %b",$time,A,B,Cin,Sum,Carry);
+        $display("time = %d: A = %b, B = %b, Cin = %b, Sum = %b, Carry = %b",$time,A,B,Cin,Sum,Carry);
     end
 
+    initial begin
+        #80
+        $finish;
+    end
 
     initial begin
         A = 1; B = 1; Cin = 0;
-        #1 
+        #5 
         $display("\n");
-        A = 200; B = 200; Cin = 1;
-        #1 
+        A = 100; B = 100; Cin = 1;
+        #5 
         $display("\n");
-        A = 32; B = 6; Cin = 1;
-        #1
+        A = 30; B = 16; Cin = 1;
+        #5
         $display("\n");
         A = 26; B = 5; Cin = 1;
-        #1
+        #5
         $display("\n");
-        A = 64; B = 64; Cin = 0;
-        #1
+        A = 32; B = 32; Cin = 0;
+        #5
         $display("\n");
-        A = 43; B = 4; Cin = 0;
-        #1
+        A = 43; B = 4; Cin = 1;
+        #5
         $display("\n");
         A = 65; B = 43; Cin = 0;
-        #1
+        #5
         $display("\n");
-        A = 128; B = 128; Cin = 1;
-        #1
+        A = 64; B = 64; Cin = 1;
+        #5
         $display("\n");
-        A = 5; B = 220; Cin = 0;
-        #1
+        A = 105; B = 220; Cin = 0;
+        #5
+        $display("\n");
+        A = 128; B = 96; Cin = 1;
+        #5
+        $display("\n");
+        A = 100; B = 96; Cin = 0;
+        #5
         $display("\n");
         A = 128; B = 96; Cin = 0;
+        #5
+        $display("\n");
+        A = 128; B = 128; Cin = 0;
+        #5
+        $display("\n");
+        A = 28; B = 96; Cin = 1;
+        #5
+        $display("\n");
+        A = 32; B = 128; Cin = 0;
     end
 
 endmodule
