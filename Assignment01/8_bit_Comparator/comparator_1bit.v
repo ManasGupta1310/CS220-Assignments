25eb4b8bf0f2c88ea32a47d707d4347a4a36ac39
hitesh-anand <82741231+hitesh-anand@users.noreply.github.com>
2022-01-23 22:06:04 +0530

    Update comparator_1bit.v
    updated after successful testing

--- Assignment01/8_bit_Comparator/comparator_1bit.v
+++ Assignment01/8_bit_Comparator/comparator_1bit.v
@@ -1,16 +1,9 @@
 module comparator_1bit(a, b, lin, ein, gin, less, equal, greater);
-    input a;
-    input b;
-    input lin;
-    input ein;
-    input gin;
+    input a, b, lin, ein, gin;
 
-    output less;
-    wire less;
-    output equal;
-    wire equal;
-    output greater;
-    wire greater;
+    output wire less;;
+    output wire equal;
+    output wire greater;
 
     assign less = lin|ein&~a&b;
     assign equal = ein&~(a^b);
