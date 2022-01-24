2372626b840a14f169c53bb027ffd563727425df
hitesh-anand <82741231+hitesh-anand@users.noreply.github.com>
2022-01-24 19:41:04 +0530

    Update A1Q3_dec_3to8_test.v
    updated the test bench file after rectifying the output errors

--- Assignment01/3_to_8_Decoder/A1Q3_dec_3to8_test.v
+++ Assignment01/3_to_8_Decoder/A1Q3_dec_3to8_test.v
@@ -1,71 +1,59 @@
-// `include "dec_3to8.v"
+`include "dec_3to8.v"
 
 module dec_3to8_test;
+    reg [2:0] a;
+    reg en;
+    wire [7:0] out;
 
-    reg a, b, c, en;
-    wire[7:0] out;
 
-    dec_3to8 dec1(out, a, b, c, en);
 
-    always @ (a or b or c or en) begin
-        $display("time = %d: c = %b, b = %b, a = %b, enable = %b, out = %b",$time,c,b,a,en,out);
+    dec_3to8 dec1(a[0], a[1], a[2], en, out);
+
+    always @(a or en) begin 
+        #1
+        $display("time = %d: a = %b, b = %b, c = %b, en = %b, out = %b\n",$time,a[2],a[1],a[0],en,out);
     end
 
     initial begin
-        #80
+        #100
         $finish;
     end
 
     initial begin
-        a = 0; b = 0; c = 0; en = 0;
-        #5 
-        $display("\n");
-        a = 1; b = 0; c = 0; en = 0;
-        #5 
-        $display("\n");
-        a = 0; b = 1; c = 0; en = 0;
+        a=3'b000; en=0;
         #5
-        $display("\n");
-        a = 1; b = 1; c = 0; en = 0;
+        a=3'b001; en=0;
         #5
-        $display("\n");
-        a = 0; b = 0; c = 1; en = 0;
+        a=3'b010; en=0;
         #5
-        $display("\n");
-        a = 1; b = 0; c = 1; en = 0;
+        a=3'b011; en=0;
         #5
-        $display("\n");
-        a = 0; b = 1; c = 1; en = 0;
+        a=3'b100; en=0;
+        #5;
+        a=3'b101; en=0;
         #5
-        $display("\n");
-        a = 1; b = 1; c = 1; en = 0;
+        a=3'b110; en=0;
         #5
-        $display("\n");
-        a = 0; b = 0; c = 0; en = 1;
+        a=3'b111; en=0;
         #5
-        $display("\n");
-        a = 1; b = 0; c = 0; en = 1;
+        a=3'b000; en=1;
         #5
-        $display("\n");
-        a = 0; b = 1; c = 0; en = 1;
+        a=3'b001; en=1;
         #5
-        $display("\n");
-        a = 1; b = 1; c = 0; en = 1;
+        a=3'b010; en=1;
         #5
-        $display("\n");
-        a = 0; b = 0; c = 1; en = 1;
+        a=3'b011; en=1;
         #5
-        $display("\n");
-        a = 1; b = 0; c = 1; en = 1;
+        a=3'b100; en=1;
+        #5;
+        a=3'b101; en=1;
         #5
-        $display("\n");
-        a = 0; b = 1; c = 1; en = 1;
+        a=3'b110; en=1;
         #5
-        $display("\n");
-        a = 1; b = 1; c = 1; en = 1;
+        a=3'b111; en=1;
     end
 
-endmodule;
+endmodule 
 
 
 
