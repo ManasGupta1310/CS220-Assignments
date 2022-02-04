70118c7dbd2651bb3c06eea3907ebc1367ebfebb
ManasGupta <mmanasggupta@gmail.com>
2022-02-04 17:19:28 +0530

    Edits

--- Assignment02/CLA_Adder/cla_adder.v
+++ Assignment02/CLA_Adder/cla_adder.v
@@ -13,7 +13,7 @@ module cla_adder(a,b,cin,sum,cout);
                                             // For any i ===> cin[i]=cout[i-1]
     // The sum at any iter will ===> sum[i]=xor(p[i],c[i-1]
 
-    
+
     assign p0=(a[0]^b[0]);  // We assign the propagators the value: p[i]=xor(a[i], b[i])
     assign p1=(a[1]^b[1]);
     assign p2=(a[2]^b[2]);
@@ -24,10 +24,10 @@ module cla_adder(a,b,cin,sum,cout);
     assign g2=(a[2]&b[2]);
     assign g3=(a[3]&b[3]);
 
-    assign c0=g0|(p0&cin);          // We assign the intermediate carry outs the value from the formula
-    assign c1=g1|(p1&c0);           // c1=g1|(p1&g0)|(p1&p0&cin);
-    assign c2=g2|(p2&c1);           // c2=g2|(p2&g1)|(p2&p1&g0)|(p1&p1&p0&cin);
-    assign c3=g3|(p3&c2);           // c3=g3|(p3&g2)|(p3&p2&g1)|(p3&p2&p1&g0)|(p3&p2&p1&p0&cin);
+    assign c0=g0|(p0&cin);  // We assign the intermediate carry outs the value from the formula
+    assign c1=g1|(p1&c0);   // c1=g1|(p1&g0)|(p1&p0&cin);
+    assign c2=g2|(p2&c1);   // c2=g2|(p2&g1)|(p2&p1&g0)|(p1&p1&p0&cin);
+    assign c3=g3|(p3&c2);   // c3=g3|(p3&g2)|(p3&p2&g1)|(p3&p2&p1&g0)|(p3&p2&p1&p0&cin);
 
 
     assign sum[0]=p0^cin,
