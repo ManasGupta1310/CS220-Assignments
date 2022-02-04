3f8a93f0951ea8be3c9353801e071a5d591de7e3
ManasGupta <mmanasggupta@gmail.com>
2022-02-04 17:09:14 +0530

    CLA_Adder editted

--- Assignment02/CLA_Adder/cla_adder.v
+++ Assignment02/CLA_Adder/cla_adder.v
@@ -1,26 +1,38 @@
 module cla_adder(a,b,cin,sum,cout);
-    input[3:0] a,b;
-    input cin;
-    output [3:0] sum;
-    output cout;
+    input[3:0] a,b;     // a and b are 4bit inputs
+    input cin;          // Carry in
+    output [3:0] sum;   // Final Sum
+    output cout;        // Final carry out
     
-    wire p0,p1,p2,p3,g0,g1,g2,g3,c1,c2,c3,c4;
-    assign p0=(a[0]^b[0]),
-    p1=(a[1]^b[1]),
-    p2=(a[2]^b[2]),
-    p3=(a[3]^b[3]);
-    assign g0=(a[0]&b[0]),
-    g1=(a[1]&b[1]),
-    g2=(a[2]&b[2]),
-    g3=(a[3]&b[3]);
-    assign c0=cin,
-    c1=g0|(p0&cin),
-    c2=g1|(p1&g0)|(p1&p0&cin),
-    c3=g2|(p2&g1)|(p2&p1&g0)|(p1&p1&p0&cin),
-    c4=g3|(p3&g2)|(p3&p2&g1)|(p3&p2&p1&g0)|(p3&p2&p1&p0&cin);
-    assign sum[0]=p0^c0,
-    sum[1]=p1^c1,
-    sum[2]=p2^c2,
-    sum[3]=p3^c3;
-    assign cout=c4;
+
+    wire p0,p1,p2,p3;   // p stands for the carry out propagator 
+    wire g0,g1,g2,g3;   // g stands for the carry out generator
+    wire c0,c1,c2,c3;   // c stands for the intermediate carry outs
+
+    //  The formula for carry out in any iteration will be ===>  cout[i]=(a[i]&b[i]) + (xor(a[i],b[i])&cin[i-1])
+                                            // For any i ===> cin[i]=cout[i-1]
+
+    assign p0=(a[0]^b[0]);  // We assign the propagators the value: p[i]=xor(a[i], b[i])
+    assign p1=(a[1]^b[1]);
+    assign p2=(a[2]^b[2]);
+    assign p3=(a[3]^b[3]);
+
+    assign g0=(a[0]&b[0]);  // We assign the generators the value: g[i]=a[i]&b[i]
+    assign g1=(a[1]&b[1]);
+    assign g2=(a[2]&b[2]);
+    assign g3=(a[3]&b[3]);
+
+    assign c0=g0|(p0&cin);          // We assign the intermediate carry outs the value from the formula
+    assign c1=g1|(p1&c0);           // c1=g1|(p1&g0)|(p1&p0&cin);
+    assign c2=g2|(p2&c1);           // c2=g2|(p2&g1)|(p2&p1&g0)|(p1&p1&p0&cin);
+    assign c3=g3|(p3&c2);           // c3=g3|(p3&g2)|(p3&p2&g1)|(p3&p2&p1&g0)|(p3&p2&p1&p0&cin);
+
+
+    assign sum[0]=p0^cin,
+    sum[1]=p1^c0,
+    sum[2]=p2^c1,
+    sum[3]=p3^c2;
+
+    assign cout=c3;
+    
 endmodule
--- Assignment02/CLA_Adder/cla_adder_tb.v
+++ Assignment02/CLA_Adder/cla_adder_tb.v
@@ -1,33 +1,71 @@
-#`include "cla_adder.v"
+`include "cla_adder.v"
+
 module TestModule;
-// Inputs
-reg [3:0] a;
-reg [3:0] b;
-reg cin;
+    // Inputs
+    reg [3:0] a;
+    reg [3:0] b;
+    reg cin;
 
-// Outputs
-wire [3:0] sum;
-wire cout;
+    // Outputs
+    wire [3:0] sum;
+    wire cout;
 
-// Instantiate the Unit Under Test (UUT)
-CLA_Adder uut (
-.a(a),
-.b(b),
-.cin(cin),
-.sum(sum),
-.cout(cout)
-);
-initial begin
-// Initialize Inputs
-a = 0;
-b = 0;
-cin = 0;
-// Wait 100 ns for global reset to finish
-#100;
-a = 5;
-b = 6;
-cin = 1;
-// Wait 100 ns for global reset to finish
-#100;
-end
+    // Instantiate the cla_adder
+    cla_adder adder (.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+
+    always @ (a or b or cin or sum or cout) begin
+        $display("time = %d: A = %b, B = %b, Cin = %b, Sum = %b, Carry = %b",$time,a,b,cin,sum,carry);
+    end
+
+    initial begin
+        #80
+        $finish;
+    end
+
+    initial begin
+        A = 1; B = 1; Cin = 0;
+        #5 
+        $display("\n");
+        A = 100; B = 100; Cin = 1;
+        #5 
+        $display("\n");
+        A = 30; B = 16; Cin = 1;
+        #5
+        $display("\n");
+        A = 26; B = 5; Cin = 1;
+        #5
+        $display("\n");
+        A = 32; B = 32; Cin = 0;
+        #5
+        $display("\n");
+        A = 43; B = 4; Cin = 1;
+        #5
+        $display("\n");
+        A = 65; B = 43; Cin = 0;
+        #5
+        $display("\n");
+        A = 64; B = 64; Cin = 1;
+        #5
+        $display("\n");
+        A = 105; B = 220; Cin = 0;
+        #5
+        $display("\n");
+        A = 128; B = 96; Cin = 1;
+        #5
+        $display("\n");
+        A = 100; B = 96; Cin = 0;
+        #5
+        $display("\n");
+        A = 128; B = 96; Cin = 0;
+        #5
+        $display("\n");
+        A = 128; B = 128; Cin = 0;
+        #5
+        $display("\n");
+        A = 28; B = 96; Cin = 1;
+        #5
+        $display("\n");
+        A = 32; B = 128; Cin = 0;
+    end
+    
 endmodule
