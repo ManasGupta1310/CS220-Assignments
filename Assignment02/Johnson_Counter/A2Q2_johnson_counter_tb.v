a279f672e8a3fa904c2ebcb3f9a4378b5ea1e5a7
ManasGupta <mmanasggupta@gmail.com>
2022-02-06 11:31:33 +0530

    Edits

--- Assignment02/Johnson_Counter/A2Q2_johnson_counter_tb.v
+++ Assignment02/Johnson_Counter/A2Q2_johnson_counter_tb.v
@@ -7,7 +7,7 @@ wire [7:0]q;    // Outputs
 
 A2Q2_johnson_counter johnson_count (rst,clk,q);     // Declaring the counter
 
-always #5 clk = ~clk;  
+always #1 clk = ~clk;  
   
   initial begin  
       //Initilaising clock and reset to 1
@@ -16,7 +16,7 @@ always #5 clk = ~clk;
   
     $monitor ("Time=%0t     Output=%b", $time, q);     // Displaying the output
 
-    repeat (2) @(posedge clk);  
+    repeat (1) @(posedge clk);  
     rst = 1;  // Setting the reset to 1
     repeat (15) @(posedge clk);  
     $finish;  
