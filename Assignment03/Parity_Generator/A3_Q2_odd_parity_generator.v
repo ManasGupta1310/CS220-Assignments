cedf279e99e0a100704f11c16e7166f60d43ccc5
hitesh-anand <82741231+hitesh-anand@users.noreply.github.com>
2022-02-17 16:52:03 +0530

    Update the file

--- Assignment03/Parity_Generator/A3_Q2_odd_parity_generator.v
+++ Assignment03/Parity_Generator/A3_Q2_odd_parity_generator.v
@@ -1,7 +1,55 @@
-module odd_parity_generator(in, p);
-input [2:0] in;
-output p;
+module fsm(inp, clk, reset, p_state, n_state, out);
 
-assign p = ~(in[0]^(in[1]^in[2]));
+input inp;
+input clk, reset;
+input [1:0] p_state;
+output reg[1:0] n_state;
+output reg out;
 
-endmodule;
+parameter S0 = 2'b00 , S1 = 2'b01 , S2 = 3'b10;  // states
+
+
+always @ (posedge clk or negedge clk)
+  begin : fsm
+  case(p_state)
+  S0 : if (inp == 1'b1) begin
+    n_state = S1;
+  end
+  else begin
+    n_state = S2;
+  end
+
+
+  S1 : if (inp == 1'b1) begin
+    n_state = S2;
+  end
+  else begin
+    n_state = S1;
+  end
+
+  S2 : if (inp == 1'b1) begin
+    n_state = S1;
+  end
+  else begin
+    n_state = S2;
+  end
+
+  default : begin 
+    n_state = S0;
+  end
+
+  endcase
+
+  if(reset == 1'b1) begin
+    out = (n_state == S2)? 1'b1 : 1'b0;
+  end
+  else
+    out = 1'bx;
+  end
+
+
+always@(posedge clk or negedge clk) begin    
+    #1 $display("At time : %d, input given is : %b, present state is : %b, next state is: %b, and the parity bit generated is : %b\n", $time, inp, p_state, n_state, out);
+end
+
+endmodule
