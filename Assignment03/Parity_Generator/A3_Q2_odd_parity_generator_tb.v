45b9472fc79ebb4d6cf9806c1c8072e6f2739403
hitesh-anand <82741231+hitesh-anand@users.noreply.github.com>
2022-02-17 16:52:45 +0530

    Update test bench
    Updated the test bench after successful testing

--- Assignment03/Parity_Generator/A3_Q2_odd_parity_generator_tb.v
+++ Assignment03/Parity_Generator/A3_Q2_odd_parity_generator_tb.v
@@ -1,27 +1,64 @@
 `include "A3_Q2_odd_parity_generator.v"
 module odd_parity_generator_tb;
 
-reg[2:0] in;  //input
-wire p;       //output
+reg clk;
+reg reset;             // detects the end of an input(kind of reset)          
+reg in;              // input
+reg[1:0] p_state;    // present state
+wire[1:0] n_state;  // next state
+wire p;              // parity bit
+fsm FSM(in, clk, reset, p_state, n_state, p);
 
-odd_parity_generator opg(in, p);
-
-//eight different inputs
 initial begin
-    #5 in = 3'b000;
-    #5 in = 3'b001;
-    #5 in = 3'b010;
-    #5 in = 3'b011;
-    #5 in = 3'b100;
-    #5 in = 3'b101;
-    #5 in = 3'b110;
-    #5 in = 3'b111;
-    #10 $finish;
+    clk = 1'b0;
+    p_state = 2'b00;
+    reset = 1'b0;
 end
 
-always@(in) begin
-    #1
-    $display("At time : %d, input given is : %b, and the parity bit generated is : %b\n", $time, in, p);
+always #5 clk = ~clk;
+
+
+initial begin
+    #5 in = 1'b0; p_state = 2'b00;
+    #5 in = 1'b0; p_state = n_state;
+    #5 in = 1'b0; reset = 1'b1; p_state = n_state;
+
+    // new input
+    #5 in = 1'b0; reset = 1'b0; p_state = 2'b00;
+    #5 in = 1'b0; p_state = n_state;
+    #5 in = 1'b1; reset = 1'b1; p_state = n_state;
+
+    // new input
+    #5 in = 1'b0;  reset = 1'b0; p_state = 2'b00;
+    #5 in = 1'b1; p_state = n_state;
+    #5 in = 1'b0; reset = 1'b1; p_state = n_state;
+
+    // new input
+    #5 in = 1'b0; reset = 1'b0; p_state = 2'b00;
+    #5 in = 1'b1; p_state = n_state;
+    #5 in = 1'b1; reset = 1'b1; p_state = n_state;
+
+    // new input
+    #5 in = 1'b1; reset = 1'b0; p_state = 2'b00;
+    #5 in = 1'b0; p_state = n_state;
+    #5 in = 1'b0; reset = 1'b1;
+
+    // new input
+    #5 in = 1'b1; reset = 1'b0; p_state = 2'b00;
+    #5 in = 1'b0; p_state = n_state;
+    #5 in = 1'b1; reset = 1'b1; p_state = n_state;
+
+    //new input
+    #5 in = 1'b1; reset = 1'b0; p_state = 2'b00;
+    #5 in = 1'b1; p_state = n_state;
+    #5 in = 1'b0; reset = 1'b1; p_state = n_state;
+
+    // new input
+    #5 in = 1'b1; reset = 1'b0; p_state = 2'b00;
+    #5 in = 1'b1; p_state = n_state;
+    #5 in = 1'b1; reset = 1'b1; p_state = n_state;
+
+    #4 $finish;
 end
 
 endmodule
