0fcbd643c9cb71644b77b1c48bffbde93e67071b
hitesh-anand <82741231+hitesh-anand@users.noreply.github.com>
2022-03-05 16:25:25 +0530

    Changed the binary encodings

--- Assignment04/3bit_gray_code_counter/A4Q1_gray_code_counter.v
+++ Assignment04/3bit_gray_code_counter/A4Q1_gray_code_counter.v
@@ -1,5 +1,5 @@
 module gray_code_counter(input clk, input in, output reg [2:0] count, output reg out);
-parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b010, S3 = 3'b011, S4 = 3'b100, S5 = 3'b101, S6 = 3'b110, S7 = 3'b111;
+parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b011, S3 = 3'b010, S4 = 3'b110, S5 = 3'b111, S6 = 3'b101, S7 = 3'b100;
 
 reg [2:0] state = 0;
 
